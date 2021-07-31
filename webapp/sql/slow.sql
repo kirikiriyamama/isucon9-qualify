e203606ddcd85816adcba108be0ad28f3a64e290
kirikiriyamama <kirikiriyamama@gmail.com>
2021-07-31 17:26:41 +0900

    slow query の閾値さげる

--- webapp/sql/slow.sql
+++ webapp/sql/slow.sql
@@ -1,4 +1,4 @@
 set global slow_query_log = 1;
 set global slow_query_log_file = '/var/log/mysql/slow.log';
-set global long_query_time = 1;
+set global long_query_time = 0.5;
 set global log_queries_not_using_indexes = 1;
